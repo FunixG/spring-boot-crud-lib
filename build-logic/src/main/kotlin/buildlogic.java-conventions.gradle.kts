629eb1ddea864727f3cf048d2f66beb8da92feeb
Antoine PRONNIER <antoine.pronnier@gmail.com>
2026-01-23 22:07:11 +0100

    1.3.3 - Fixed publication on GitHub packages

--- build-logic/src/main/kotlin/buildlogic.java-conventions.gradle.kts
+++ build-logic/src/main/kotlin/buildlogic.java-conventions.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "dev.funixgaming.spring.core"
-version = "1.3.2"
+version = "1.3.3"
 
 extra["springCloudVersion"] = "2025.1.0"
 java.sourceCompatibility = JavaVersion.VERSION_25
