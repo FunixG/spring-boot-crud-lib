7299e8b179ca24cfb3673c4a54cd11e0dadbdc79
Antoine PRONNIER <antoine.pronnier@gmail.com>
2026-02-14 12:07:17 +0100

    fix build

--- storage/build.gradle.kts
+++ storage/build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
-    id("buildlogic-spring-core-funix.java-conventions")
+    id("buildlogic-spring-service.java-conventions")
 }
 
 group = "dev.funixgaming.spring.core.storage"
 description = "storage"
 
 dependencies {
+    api(project(":crud"))
     implementation(libs.org.imgscalr.imgscalr.lib)
 }
