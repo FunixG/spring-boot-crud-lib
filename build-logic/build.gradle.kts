02f59d7c914b9c0852757c56933f47cf629b27bf
Antoine PRONNIER <antoine.pronnier@gmail.com>
2026-02-15 11:22:46 +0100

    1.3.11 - add missing plugins on github packages

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -19,3 +19,33 @@ dependencies {
     implementation(libs.findLibrary("org.spring.boot.gradle.plugin").get())
     implementation(libs.findLibrary("org.flywaydb.flyway.gradle.plugin").get())
 }
+
+gradlePlugin {
+    plugins {
+        create("springConventions") {
+            id = "dev.funixgaming.spring.conventions"
+            implementationClass = "Dev_funixgaming_spring_conventionsPlugin"
+        }
+        create("springConventionsService") {
+            id = "dev.funixgaming.spring.conventions.service"
+            implementationClass = "Dev_funixgaming_spring_conventions_servicePlugin"
+        }
+        create("springConventionsPostgres") {
+            id = "dev.funixgaming.spring.conventions.postgres"
+            implementationClass = "Dev_funixgaming_spring_conventions_postgresPlugin"
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/FunixG/spring-boot-crud-lib")
+            credentials {
+                username = "FunixG"
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
